11ee76321632b83df2802d34fffa02de1c41498c
Musab Obada Y. Elsamani <musab0124887085@gmail.com>
2022-05-23 22:31:51 +0200

    Update dino.js

--- js/dino.js
+++ js/dino.js
@@ -4,7 +4,7 @@ import {
   incrementCustomProperty,
 } from "./updateCustomProperty.js";
 const JUMP_SPEED = 0.45,
-  GRAVITY = 0.002,
+  GRAVITY = 0.0015,
   DINO_FRAME_COUNT = 2,
   FRAME_TIME = 100,
   dinoElement = document.querySelector("[data-dino]");
